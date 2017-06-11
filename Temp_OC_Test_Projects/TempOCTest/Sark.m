43abc2b645af0377ed26841d34d7d9eba24d4414
冯生伟 <247159603@qq.com>
2017-06-11 23:04:24 +0800

    no message

---
+++ Temp_OC_Test_Projects/TempOCTest/Sark.m
@@ -0,0 +1,28 @@
+//
+//  Sark.m
+//  TempOCTest
+//
+//  Created by JZ_Stone on 2017/6/11.
+//  Copyright © 2017年 JZ_Stone. All rights reserved.
+//
+
+#import "Sark.h"
+
+@implementation Sark
+
+- (void)speak
+{
+    
+}
+
+@end
+
+
+@implementation Sark (GayExtention)
+
+- (void)burst
+{
+    
+}
+
+@end
---
+++ Temp_OC_Test_Projects/TempOCTest/main.m
@@ -0,0 +1,91 @@
+//
+//  main.m
+//  TempOCTest
+//
+//  Created by JZ_Stone on 2017/6/9.
+//  Copyright © 2017年 JZ_Stone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (plus)
+
+- (NSNumber *)factorial;
+
+@end
+
+
+@implementation NSNumber (plus)
+
+- (NSNumber *)factorial
+{
+    //    return @(tgamma([self doubleValue] + 1));
+    return @(fabs([self doubleValue]+1));
+}
+
+@end
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        // NSExpression
+        //==============================================================================
+        NSExpression *expression1 = [NSExpression expressionWithFormat:@"FUNCTION(100, 'factorial')"];
+        id value1 = [expression1 expressionValueWithObject:nil context:nil];
+        NSLog(@"NSExpression vlue1 %@",value1);
+        
+        
+        NSArray*numbers = @[@1,@2,@3,@4,@5,@6];
+        NSExpression *expression2 = [NSExpression expressionForFunction:@"now" arguments:@[[NSExpression expressionForConstantValue:numbers]]];
+        id value2 = [expression2 expressionValueWithObject:nil context:nil];
+        NSLog(@"NSExpression value2 %@",value2);
+        
+        //        NSArray *numbers1 = @[@100, @22, @33, @12, @1000, @99, @44];
+        NSExpression *expression3 = [NSExpression expressionForFunction:@"random" arguments:@[[NSExpression expressionForConstantValue:@22]]];
+        NSLog(@"NSExpression value3 %@", [expression3 expressionValueWithObject:nil context:nil]); // => 3.21859...
+        //==============================================================================
+        
+        
+        // NSNumber
+        //==============================================================================
+        NSString *str1 = @"89480.9520760";
+        NSLog(@"value %@", @([str1 doubleValue]));
+        
+        
+        // GCD
+        //==============================================================================
+        NSLog(@"1");
+        dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            sleep(10);
+            NSLog(@"2");
+        });
+        NSLog(@"3");
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
